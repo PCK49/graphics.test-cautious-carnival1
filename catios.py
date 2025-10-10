e9138fe049ebaee509eee9eb01a36cdd328c3a41
PCK49 <160991878+PCK49@users.noreply.github.com>
2025-10-09 20:13:14 -0700

    Create catios.py

---
+++ catios.py
@@ -0,0 +1,27 @@
+#app.py
+from flask import Flask, request, redirect, render_template_string
+
+app=Flask(__name__)
+
+ninersite="https://49ers.com"
+
+TEMPLATE="""
+<!DOCTYPE html>
+<html>
+  <body>
+    <p>only secret get this...</p>
+    <form action="/submit" method="post">
+    <input type="text" name="name" placeholder="keyword">
+    <input type="submit" value="submit">
+    </form>
+    </body>
+</html>"""
+
+@app.route("/")
+def homeforniner():
+  return render_template_string(TEMPLATE)
+@app.route("/submit",methods=["POST"])
+def submit():
+    return redirect(ninersite)
+if __name__ == "__main__":
+  app.run(debug=True)
