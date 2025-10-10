d785a98d033c14546df76b39732fdf37165ee21c
PCK49 <160991878+PCK49@users.noreply.github.com>
2025-10-10 13:48:52 -0700

    Update onlysecert.js

--- onlysecert.js
+++ onlysecert.js
@@ -1,1 +1,10 @@
-function 
+function niners() {
+  let pass=prompt("enter the passcode");
+  if (pass=="ninerfan49") {
+    alert("you got in! other one couldnt work cause static stuff");
+    window.location.href = "https://49ers.com";
+  }
+  else {
+    alert("better luck next time");
+  }
+}
